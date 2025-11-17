b5581b517531714cd74785fb2ac09715a0e2059f
beatriz-augusto <beatriz.augusto@sptech.school>
2025-11-16 22:45:24 -0300

    Modificações na api para o mural de avaliações

---
+++ src/routes/mural.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+
+var avisoController = require("../controllers/avisoController");
+
+// router.get("/listar", function (req, res) {
+//     avisoController.listar(req, res);
+// });
+
+// router.get("/listar/:idUsuario", function (req, res) {
+//     avisoController.listarPorUsuario(req, res);
+// });
+
+// router.get("/pesquisar/:descricao", function (req, res) {
+//     avisoController.pesquisarDescricao(req, res);
+// });
+
+// router.post("/publicar/:idUsuario", function (req, res) {
+//     avisoController.publicar(req, res);
+// });
+
+// router.put("/editar/:idAviso", function (req, res) {
+//     avisoController.editar(req, res);
+// });
+
+// router.delete("/deletar/:idAviso", function (req, res) {
+//     avisoController.deletar(req, res);
+// });
+
+router.post("/publicar/:idUsuario", function (req, res) {
+    avaliacaoController.publicar(req, res);
+ });
+
+ router.get("/listar/:idUsuario", function (req, res) {
+    avaliacaoController.listarPorFilme(req, res);
+});
+
+router.delete("/deletar/:idAvaliacoes", function (req, res) {
+    avaliacaoController.deletar(req, res);
+ });
+
+module.exports = router;
